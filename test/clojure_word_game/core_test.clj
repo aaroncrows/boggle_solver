750b37561147b7b334532e7788a7816715370977
Aaron Krause <aaronjkrause@gmail.com>
2017-01-10 15:45:36 -0800

    lein boilerplate

---
+++ test/clojure_word_game/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-word-game.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-word-game.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
