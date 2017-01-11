1c47df1fe4f375f7543b7b8a1592ea606aa76d77
Aaron Krause <aaronjkrause@gmail.com>
2017-01-10 16:02:15 -0800

    got word list

--- src/clojure_word_game/core.clj
+++ src/clojure_word_game/core.clj
@@ -1,7 +1,11 @@
 (ns clojure-word-game.core
   (:gen-class))
 
+(require '[clojure.string :as s])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (slurp "/usr/share/dict/words"))
+
+(set (filter #(= % (s/lower-case %)) (s/split (slurp "/usr/share/dict/words") #"\n")))
