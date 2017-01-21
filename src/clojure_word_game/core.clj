b1abf29fcefee836a75185c3f4eac0b077e24097
Aaron Krause <aaronjkrause@gmail.com>
2017-01-20 17:50:10 -0800

    function to get all coordinate paris for grid

--- src/clojure_word_game/core.clj
+++ src/clojure_word_game/core.clj
@@ -2,11 +2,7 @@
   (:gen-class))
 
 (require '[clojure.string :as s])
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (slurp "/usr/share/dict/words"))
+(declare str-to-keys)
 
 (defn generate-word-set
   []
@@ -14,28 +10,12 @@
     #(= % (s/lower-case %))
     (s/split (slurp "/usr/share/dict/words")
     #"\n")))
+(def w (generate-word-set))
 
-(def pw (partition (/ (count w) 10) (generate-word-set)))
-
-#_(defn generate-trie
-  [words]
-  (def make-key (comp keyword str))
-  (let [trie {}]
-    (loop [[curr & left] words
-            trie]
-      (loop [[cur-char & remain-char] curr
-              trie]
-
-        (when (not (nil? cur-char))
-          (do
-            (when (nil? ((make-key cur-char) trie))
-            (recur remain-char))))
-    (if (nil? curr)
-      trie
-    (do
-    (recur left)))))
-
-))
+(defn str-to-keys
+  "Turns a string into a seq of keywords"
+  [string]
+  (map #(-> % str keyword) string))
 
 (defn random-letter
   "Returns a random capital letter from A-Z"
@@ -48,17 +28,15 @@
   (mapv random-letter (range length)))
 
 (defn make-trie
-  ([words trie]
+  [words]
   (loop [[curr & rest] words trie {}]
     (if (nil? curr)
      trie
      (recur rest (update-in trie
-      (map #(keyword (str %)) (seq curr))
+      (str-to-keys curr)
       assoc :word curr)))))
-    ([words] make-trie words {}))
-(def trie (make-trie (generate-word-set)))
 
-(reduce {} make-trie (partition (/ (count w) 50) (generate-word-set)))
+(def trie (make-trie (generate-word-set)))
 
 (defn make-board
   "makes a game board of random letters"
@@ -66,13 +44,18 @@
   (vec (for [x (range s)]
     (random-letter-vec s))))
 
-(def board (make-grid 3))
+(def board (make-board 3))
 
 (defn adjacent-coords
-  [[x y]]
+  [[y x]]
   (for [mod-y [-1 0 1] mod-x [-1 0 1]
           :when (not-every? #(= 0 %) [mod-x mod-y])]
           [(+ mod-y y)(+ mod-x x)]))
+(defn grid-coords
+  [grid]
+  (for [[y row] (map-indexed vector grid)
+        [x val] (map-indexed vector row)]
+  [y x]))
 
 (defn each-adjacent
   [f grid coords]
@@ -81,21 +64,27 @@
   (loop [[adjacent & remaining-adjacent] adjacent-coords]
     (if adjacent
       (do (f (get-in grid adjacent))
-        (recur remaining-adjacent))
-      match)))
-
-(defn str-to-keys
-  "Turns a string into a seq of keywords"
-  [string]
-  (map #(-> % str keyword) string))
+        (recur remaining-adjacent)))))
 
 (defn is-prefix?
   "determines if string is a valid prefix"
   [trie string]
   (->> string str-to-keys (get-in trie) boolean))
+(defn in-vec?
+  [sub vec]
+  (some #(= sub %) vec))
+
+(defn list-matches
+  [grid coords]
+  (loop [match-words []]
+  (loop [[adjacent & remaining-adjacent] (adjacent-coords coords) visited [] string (get-in grid coords)]
+    (if (or (not adjacent) (in-vec? adjacent visited))
+      (conj match-words string)
+      (recur remaining-adjacent (conj visited adjacent) (str string (get-in grid adjacent)))
+      #_(recur (adjacent-coords adjacent) [adjacent] "")
+))))
 
 
-#_(each-adjacent #(print %) [[1 2 3][4 5 6][7 8 9]] [1 1])
-(def t {:a {:b {:c {:word "ab"}}}})
-(is-prefix? trie "baby")
+#_(list-matches [[1 2][3 4]] [1 0])
 
+(print (grid-coords [[1 2][3 4]]))
